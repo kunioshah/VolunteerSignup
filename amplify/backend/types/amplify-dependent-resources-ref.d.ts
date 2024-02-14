f3a014e2775603b1c13a42cd63aa56b592fb4abf
kunioshah <kunioshah@gmail.com>
2024-02-14 00:06:16 -0800

    added storage and image

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s39dc39d0f": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -27,7 +27,9 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://vek5khysi5a6hfvv6fqmetbesy.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-5x4lfd2mr5ff3k5pxspnm466ta"
+    "aws_appsync_apiKey": "da2-5x4lfd2mr5ff3k5pxspnm466ta",
+    "aws_user_files_s3_bucket": "volunteersignup4a866ba665444e18bed19983888f579660028-staging",
+    "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
 
