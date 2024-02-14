c7f75e9e9c59054cb1b4d1822b21bfdc3d4797ad
kunioshah <kunioshah@gmail.com>
2024-02-13 22:46:48 -0800

    remove social provider auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,10 +3,8 @@ export type AmplifyDependentResourcesAttributes = {
     "volunteersignup8d13b40f": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
-      "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
-      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
